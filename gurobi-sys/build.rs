4584ddd58a73601cd8b4214f0e00ed3717587cf8
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-07-26 04:02:59 +0900

    fix build script

--- gurobi-sys/build.rs
+++ gurobi-sys/build.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::path::{Path, PathBuf};
+
+#[cfg(all(target_os="windows", target_env="msvc"))]
+fn setup_from_env() {
+  let gurobi_home = env::var("GUROBI_HOME").unwrap();
+  assert!(Path::new(gurobi_home.as_str()).exists());
+
+  let mut libpath: PathBuf = PathBuf::from(gurobi_home);
+  libpath.push("lib");
+
+  println!("cargo:rustc-link-search=native={}",
+           libpath.to_str().unwrap());
+}
+
+#[cfg(not(all(target_os="windows", target_env="msvc")))]
+fn setup_from_env() {}
+
+
 fn main() {
+  setup_from_env();
   println!("cargo:rustc-link-lib={}", "gurobi65")
 }

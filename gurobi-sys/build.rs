c5e0bd90f3a5c408a3414b23c1ea195c77c0827b
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-08-28 04:10:08 +0900

    estimate the version of gurobi at compile time

--- gurobi-sys/build.rs
+++ gurobi-sys/build.rs
@@ -1,19 +1,49 @@
+use std::env;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
-fn print_gurobi_confs() {
-  use std::env;
-  use std::path::{Path, PathBuf};
 
+fn gurobi_home() -> String {
   let gurobi_home = env::var("GUROBI_HOME").unwrap();
   assert!(Path::new(gurobi_home.as_str()).exists());
+  gurobi_home
+}
 
-  let mut libpath: PathBuf = PathBuf::from(gurobi_home);
-  libpath.push("lib");
+fn append_path(addpath: &str) {
+  if let Some(path) = env::var_os("PATH") {
+    let mut paths = env::split_paths(&path).collect::<Vec<_>>();
+    paths.push(PathBuf::from(addpath));
+    let new_path = env::join_paths(paths).unwrap();
+    env::set_var("PATH", &new_path);
+  }
+}
 
-  println!("cargo:rustc-link-search=native={}", libpath.to_str().unwrap());
+fn get_version_triple() -> (i32, i32, i32) {
+  let mut binpath = PathBuf::from(gurobi_home());
+  binpath.push("bin");
+  append_path(binpath.to_str().unwrap());
+
+  let output = Command::new("gurobi_cl").arg("--version").output().expect("failed to execute gurobi_cl");
+  let verno: Vec<_> = String::from_utf8(output.stdout)
+    .unwrap()
+    .split_whitespace()
+    .nth(3)
+    .unwrap()
+    .split(".")
+    .map(|s| s.parse().unwrap())
+    .collect();
+
+  (verno[0], verno[1], verno[2])
 }
 
 fn main() {
-  print_gurobi_confs();
+  let mut libpath: PathBuf = PathBuf::from(gurobi_home());
+  libpath.push("lib");
+  let libpath = libpath.to_str().unwrap();
 
-  println!("cargo:rustc-link-lib={}", "gurobi65")
+  let (major, minor, _) = get_version_triple();
+  let libname = format!("gurobi{}{}", major, minor);
+
+  println!("cargo:rustc-link-search=native={}", libpath);
+  println!("cargo:rustc-link-lib={}", libname);
 }

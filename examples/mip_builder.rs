4eb92fc3e254f9b5c37edff8e83f318aed52b2b5
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-09-07 06:25:58 +0900

    update

--- examples/mip_builder.rs
+++ examples/mip_builder.rs
@@ -9,86 +9,104 @@ use gurobi::*;
 macro_rules! def_var {
   ($model:expr, $name:ident : binary) => {
     let $name = $model.add_var(stringify!($name), Binary, 0.0, 0.0, 1.0, &[], &[]).unwrap();
-    $model.update().unwrap();
   };
   
   ($model:expr, $name:ident : integer [$lb:expr, $ub:expr]) => {
     let $name = $model.add_var(stringify!($name), Integer, 0.0, ($lb).into(), ($ub).into(), &[], &[]).unwrap();
-    $model.update().unwrap();
   };
 
   ($model:expr, $name:ident : real [$lb:expr, $ub:expr]) => {
     let $name = $model.add_var(stringify!($name), Continuous, 0.0, ($lb).into(), ($ub).into(), &[], &[]).unwrap();
-    $model.update().unwrap();
   };
 
-  ($model:expr, $name:ident : binary; $($t:tt)*) => {
+  ($model:expr, $name:ident : binary, $($t:tt)*) => {
     let $name = $model.add_var(stringify!($name), Binary, 0.0, 0.0, 1.0, &[], &[]).unwrap();
     def_var!($model, $($t)*);
-    $model.update().unwrap();
   };
   
-  ($model:expr, $name:ident : integer [$lb:expr, $ub:expr]; $($t:tt)*) => {
+  ($model:expr, $name:ident : integer [$lb:expr, $ub:expr], $($t:tt)*) => {
     let $name = $model.add_var(stringify!($name), Integer, 0.0, ($lb).into(), ($ub).into(), &[], &[]).unwrap();
     def_var!($model, $($t)*);
-    $model.update().unwrap();
   };
 
-  ($model:expr, $name:ident : real [$lb:expr, $ub:expr]; $($t:tt)*) => {
+  ($model:expr, $name:ident : real [$lb:expr, $ub:expr], $($t:tt)*) => {
     let $name = $model.add_var(stringify!($name), Continuous, 0.0, ($lb).into(), ($ub).into(), &[], &[]).unwrap();
     def_var!($model, $($t)*);
-    $model.update().unwrap();
   };
 }
 
 macro_rules! add_constr {
-  { $model:expr, $name:ident : $lhs:tt <= $rhs:tt; } => {
+  { $model:expr, $name:ident : $lhs:tt <= $rhs:tt } => {
     $model.add_constr(stringify!($name), $lhs, Less, ($rhs).into()).unwrap();
-    $model.update().unwrap();
   };
 
-  { $model:expr, $name:ident : $lhs:tt == $rhs:tt; } => {
+  { $model:expr, $name:ident : $lhs:tt == $rhs:tt } => {
     $model.add_constr(stringify!($name), $lhs, Equal, ($rhs).into()).unwrap();
-    $model.update().unwrap();
   };
 
-  { $model:expr, $name:ident : $lhs:tt >= $rhs:tt; } => {
+  { $model:expr, $name:ident : $lhs:tt >= $rhs:tt } => {
     $model.add_constr(stringify!($name), $lhs, Greater, ($rhs).into()).unwrap();
-    $model.update().unwrap();
   };
 
-  ($model:expr, $name:ident : $lhs:tt <= $rhs:tt; $($s:tt)*) => {
+  ($model:expr, $name:ident : $lhs:tt <= $rhs:tt, $($s:tt)*) => {
     $model.add_constr(stringify!($name), $lhs, Less, ($rhs).into()).unwrap();
     add_constr!($model, $($s)*);
-    $model.update().unwrap();
   };
 
-  ($model:expr, $name:ident : $lhs:tt == $rhs:tt; $($s:tt)*) => {
+  ($model:expr, $name:ident : $lhs:tt == $rhs:tt, $($s:tt)*) => {
     $model.add_constr(stringify!($name), $lhs, Equal, ($rhs).into()).unwrap();
     add_constr!($model, $($s)*);
-    $model.update().unwrap();
   };
 
-  ($model:expr, $name:ident : $lhs:tt >= $rhs:tt; $($s:tt)*) => {
+  ($model:expr, $name:ident : $lhs:tt >= $rhs:tt, $($s:tt)*) => {
     $model.add_constr(stringify!($name), $lhs, Greater, ($rhs).into()).unwrap();
     add_constr!($model, $($s)*);
-    $model.update().unwrap();
   };
 }
 
+macro_rules! def_model {
+  { env:  $env:ident,
+    name: $name:expr,
+    vars: { $($t:tt)* },
+    objective: { $($o:tt)* },
+    constrs: { $($c:tt)* }
+  } => { {
+    let mut model = Model::new($name, &$env).unwrap();
+
+    def_var!(model, $($t)*);
+    model.update().unwrap();
+
+    add_constr!(model, $($c)*);
+    model.update().unwrap();
+
+    model.set_objective($($o)*).unwrap();
+    model.update().unwrap();
+
+    model
+  } }
+}
+
 fn main() {
-  let env = Env::new("mip.log").unwrap();
-  let mut model = Model::new("mip", &env).unwrap();
+  let env = Env::new("mip_build.log").unwrap();
 
-  def_var!(model, x:binary; y:binary; z:binary; s:integer[0,2]; t:real[0,10]);
+  let model = def_model!{
+    env: env,
+    name: "mip_build",    
+    vars: {
+      x: binary,
+      y: binary,
+      z: binary,
+      s: integer[0, 2],
+      t: real[0,10]
+    },
+    objective: { &x + &y + 2.0 * &z, Maximize },
+    constrs: {
+      c0: (&x + 2.0 * &y + 3.0 * &z) <= 4,
+      c1: (&x + &y) <= 1,
+      c2: (1.0 * &s) == 0,
+      c3: (1.0 * &t) == 0
+    }
+  };
 
-  model.set_objective(&x + &y + 2.0 * &z, Maximize).unwrap();
-
-  add_constr! { model,
-    c0: (&x + 2.0 * &y + 3.0 * &z) <= 4;
-    c1: (&x + &y) <= 1;
-  }
-
-  model.update().unwrap();
   model.write("mip.lp").unwrap();
 }

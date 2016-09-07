f28b33d49443b65262fc4a15fef7954817e581a9
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2016-09-07 15:45:35 +0900

    refactor

--- examples/mip_builder.rs
+++ examples/mip_builder.rs
@@ -7,17 +7,8 @@ extern crate gurobi;
 use gurobi::*;
 
 macro_rules! def_var {
-  ($model:expr, $name:ident : binary) => {
-    let $name = $model.add_var(stringify!($name), Binary, 0.0, 0.0, 1.0, &[], &[]).unwrap();
-  };
-  
-  ($model:expr, $name:ident : integer [$lb:expr, $ub:expr]) => {
-    let $name = $model.add_var(stringify!($name), Integer, 0.0, ($lb).into(), ($ub).into(), &[], &[]).unwrap();
-  };
-
-  ($model:expr, $name:ident : real [$lb:expr, $ub:expr]) => {
-    let $name = $model.add_var(stringify!($name), Continuous, 0.0, ($lb).into(), ($ub).into(), &[], &[]).unwrap();
-  };
+  ($model:expr) => ();
+  ($model:expr,) => ();
 
   ($model:expr, $name:ident : binary, $($t:tt)*) => {
     let $name = $model.add_var(stringify!($name), Binary, 0.0, 0.0, 1.0, &[], &[]).unwrap();
@@ -36,30 +27,21 @@ macro_rules! def_var {
 }
 
 macro_rules! add_constr {
-  { $model:expr, $name:ident : $lhs:tt <= $rhs:tt } => {
-    $model.add_constr(stringify!($name), $lhs, Less, ($rhs).into()).unwrap();
-  };
+  ($model:expr) => ();
+  ($model:expr,) => ();
 
-  { $model:expr, $name:ident : $lhs:tt == $rhs:tt } => {
-    $model.add_constr(stringify!($name), $lhs, Equal, ($rhs).into()).unwrap();
-  };
-
-  { $model:expr, $name:ident : $lhs:tt >= $rhs:tt } => {
-    $model.add_constr(stringify!($name), $lhs, Greater, ($rhs).into()).unwrap();
-  };
-
-  ($model:expr, $name:ident : $lhs:tt <= $rhs:tt, $($s:tt)*) => {
-    $model.add_constr(stringify!($name), $lhs, Less, ($rhs).into()).unwrap();
+  ($model:expr, $name:ident : $lhs:tt <= $rhs:tt , $($s:tt)*) => {
+    $model.add_constr(stringify!($name), ($lhs), Less, ($rhs).into()).unwrap();
     add_constr!($model, $($s)*);
   };
 
-  ($model:expr, $name:ident : $lhs:tt == $rhs:tt, $($s:tt)*) => {
-    $model.add_constr(stringify!($name), $lhs, Equal, ($rhs).into()).unwrap();
+  ($model:expr, $name:ident : $lhs:tt == $rhs:tt , $($s:tt)*) => {
+    $model.add_constr(stringify!($name), ($lhs), Equal, ($rhs).into()).unwrap();
     add_constr!($model, $($s)*);
   };
 
-  ($model:expr, $name:ident : $lhs:tt >= $rhs:tt, $($s:tt)*) => {
-    $model.add_constr(stringify!($name), $lhs, Greater, ($rhs).into()).unwrap();
+  ($model:expr, $name:ident : $lhs:tt >= $rhs:tt , $($s:tt)*) => {
+    $model.add_constr(stringify!($name), ($lhs), Greater, ($rhs).into()).unwrap();
     add_constr!($model, $($s)*);
   };
 }
@@ -71,12 +53,7 @@ macro_rules! set_objective {
 }
 
 macro_rules! def_model {
-  { env:  $env:ident,
-    name: $name:expr,
-    vars: { $($t:tt)* },
-    objective: { $($o:tt)* },
-    constrs: { $($c:tt)* }
-  } => { {
+  { env: $env:ident,  name: $name:expr,  vars: { $($t:tt)* },  objective: { $($o:tt)* },  constrs: { $($c:tt)* } } => { {
     let mut model = Model::new($name, &$env).unwrap();
 
     def_var!(model, $($t)*);
@@ -97,13 +74,13 @@ fn main() {
 
   let model = def_model! {
     env: env,
-    name: "mip_build",    
+    name: "mip_build",
     vars: {
       x: binary,
       y: binary,
       z: binary,
       s: integer[0, 2],
-      t: real[0,10]
+      t: real[0, 10],
     },
     objective: {
       expr: (&x + &y + 2.0 * &z),
@@ -113,7 +90,7 @@ fn main() {
       c0: (&x + 2.0 * &y + 3.0 * &z) <= 4,
       c1: (&x + &y) <= 1,
       c2: (1.0 * &s) == 0,
-      c3: (1.0 * &t) == 0
+      c3: (1.0 * &t) == 0,
     }
   };
 

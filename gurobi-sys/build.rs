77aac2b6d3e9ccf39581d2d7b7f1684220c93bdb
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-02-14 00:24:43 +0900

    modify gurobi_sys/build.rs

--- gurobi-sys/build.rs
+++ gurobi-sys/build.rs
@@ -1,55 +1,48 @@
 use std::env;
 use std::path::{Path, PathBuf};
 use std::process::Command;
-use std::io::{self, Error, ErrorKind};
 
-fn gurobi_home() -> io::Result<String> {
-  env::var("GUROBI_HOME")
-    .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))
-    .and_then(|p| if Path::new(p.as_str()).exists() {
-      Ok(p)
-    } else {
-      Err(Error::new(ErrorKind::Other, "".to_owned()))
-    })
+fn gurobi_home() -> String {
+  let var = env::var("GUROBI_HOME").expect("failed to retrieve the value of GUROBI_HOME");
+  if !Path::new(&var).exists() {
+    panic!("GUROBI_HOME is invalid path");
+  }
+  var
 }
 
-fn append_path(addpath: &str) {
-  if let Some(path) = env::var_os("PATH") {
-    let mut paths = env::split_paths(&path).collect::<Vec<_>>();
-    paths.push(PathBuf::from(addpath));
-    let new_path = env::join_paths(paths).unwrap();
-    env::set_var("PATH", &new_path);
-  }
+fn append_path(addpath: PathBuf) {
+  let path = env::var_os("PATH").expect("failed to retrieve the value of PATH");
+  let mut paths: Vec<_> = env::split_paths(&path).collect();
+
+  paths.push(addpath);
+
+  let new_path = env::join_paths(paths).unwrap();
+  env::set_var("PATH", &new_path);
 }
 
 fn get_version_triple() -> (i32, i32, i32) {
-  let mut binpath = PathBuf::from(gurobi_home().unwrap());
-  binpath.push("bin");
-  append_path(binpath.to_str().unwrap());
+  append_path(PathBuf::from(gurobi_home()).join("bin"));
 
   let output = Command::new("gurobi_cl").arg("--version").output().expect("failed to execute gurobi_cl");
-  let verno: Vec<_> = String::from_utf8(output.stdout)
-    .unwrap()
+  let verno: Vec<_> = String::from_utf8_lossy(&output.stdout)
+    .into_owned()
     .split_whitespace()
     .nth(3)
-    .unwrap()
+    .expect("failed to get version string")
     .split(".")
-    .map(|s| s.parse().unwrap())
+    .map(|s| s.parse().expect("failed to parse version tuple"))
     .collect();
 
   (verno[0], verno[1], verno[2])
 }
 
 fn main() {
-  if let Ok(gurobi_home) = gurobi_home() {
-    let mut libpath: PathBuf = PathBuf::from(gurobi_home);
-    libpath.push("lib");
-    let libpath = libpath.to_str().unwrap();
+  let gurobi_home = gurobi_home();
+  let libpath: PathBuf = PathBuf::from(gurobi_home).join("lib");
 
-    let (major, minor, _) = get_version_triple();
-    let libname = format!("gurobi{}{}", major, minor);
+  let (major, minor, _) = get_version_triple();
+  let libname = format!("gurobi{}{}", major, minor);
 
-    println!("cargo:rustc-link-search=native={}", libpath);
-    println!("cargo:rustc-link-lib={}", libname);
-  }
+  println!("cargo:rustc-link-search=native={}", libpath.display());
+  println!("cargo:rustc-link-lib={}", libname);
 }

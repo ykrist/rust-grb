07e5448060063c933f52dfe3127619e4887a80c7
Me <Me>
2024-05-11 16:52:00 +1000

    fix: update compile tests

--- tests/compile-tests/user_cuts_deprecated.rs
+++ tests/compile-tests/user_cuts_deprecated.rs
@@ -6,13 +6,12 @@ use grb::prelude::*;
 fn callback(w: Where) -> CbResult {
     match w {
         Where::MIPSol(ctx) => {
-            if let Where::MIPSol(ctx) = w {
-                ctx.add_cut(c!(0 == 0))?;
-            }
+            ctx.add_cut(c!(0 == 0))?;
         }
         Where::MIPNode(ctx) => {
             ctx.add_cut(c!(0 == 0))?;
         }
+        _ => {}
     }
     Ok(())
 }

cef29f94cb9bd6adfffffd03c131a45f3baeb4f0
Me <Me>
2024-05-11 16:13:48 +1000

    fix: re-run build script if environment variables change

--- grb-sys2/build.rs
+++ grb-sys2/build.rs
@@ -48,23 +48,28 @@ fn get_lib_name(gurobi_home: Option<&PathBuf>) -> String {
   if let Ok(value) = env::var("GUROBI_LIBNAME") {
     return value;
   }
-  match gurobi_home {
-    Some(p) => match try_guess_libname(p) {
-      Ok(name) => {
-        println!("inferring libname {name:?}, set GUROBI_LIBNAME to override");
-        return name;
-      }
-      Err(e) => {
-        println!("cargo:warning={}", e);
-      }
-    },
-    None => {}
+  if cfg!(target_os = "linux") {
+    match gurobi_home {
+      Some(p) => match try_guess_libname(p) {
+        Ok(name) => {
+          println!("inferring libname {name:?}, set GUROBI_LIBNAME to override");
+          return name;
+        }
+        Err(e) => {
+          println!("cargo:warning={}", e);
+        }
+      },
+      None => {}
+    }
   }
   println!("cargo:warning=using default libname {DEFAULT_GUROBI_LIBNAME:?}, set GUROBI_LIBNAME to override");
   String::from(DEFAULT_GUROBI_LIBNAME)
 }
 
 fn main() {
+  println!("cargo:rerun-if-env-changed=GUROBI_HOME");
+  println!("cargo:rerun-if-env-changed=GUROBI_LIBNAME");
+
   let gurobi_home = try_gurobi_home();
   match gurobi_home.as_ref() {
     Ok(path) => {

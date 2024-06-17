62a6d64ec1540286f511643c8dd556039f287c16
Me <Me>
2024-06-17 20:29:44 +1000

    fix: infer library path and name correctly on Windows, add tests.

--- grb-sys2/build.rs
+++ grb-sys2/build.rs
@@ -1,27 +1,98 @@
-use std::env;
-use std::path::PathBuf;
-
+// false positives due this being a lil hacky
+/// This file exists here so that we can write tests for the build script
+/// without having to publish a separate crate.
 use anyhow::Context;
+use std::{
+    env,
+    ffi::OsStr,
+    path::{Path, PathBuf},
+};
 
 const DEFAULT_GUROBI_LIBNAME: &str = "gurobi100";
 
-fn try_gurobi_home() -> anyhow::Result<PathBuf> {
+#[derive(Clone, Copy, Debug)]
+struct GurobiLibAffixes {
+    prefix: &'static str,
+    suffix: &'static str,
+}
+
+impl GurobiLibAffixes {
+    fn match_path_for_libname(&self, filename: &OsStr) -> Option<String> {
+        let filename = filename.to_str()?;
+        let version = filename
+            .strip_prefix(self.prefix)?
+            .strip_suffix(self.suffix)?;
+        if version.bytes().all(|b| (b'0'..=b'9').contains(&b)) {
+            Some(format!("gurobi{version}"))
+        } else {
+            None
+        }
+    }
+}
+
+const DEFAULT_AFFIXES: GurobiLibAffixes = GurobiLibAffixes {
+    prefix: "libgurobi",
+    suffix: ".so",
+};
+
+const WINDOWS_AFFIXES: GurobiLibAffixes = GurobiLibAffixes {
+    prefix: "gurobi",
+    suffix: ".lib",
+};
+
+fn match_path_for_libname(filename: &OsStr) -> Option<String> {
+    let affixes = if cfg!(windows) {
+        WINDOWS_AFFIXES
+    } else {
+        DEFAULT_AFFIXES
+    };
+    affixes.match_path_for_libname(filename)
+}
+
+fn try_guess_libname(gurobi_libpath: &Path) -> anyhow::Result<String> {
+    for f in std::fs::read_dir(gurobi_libpath)? {
+        let f = f?.file_name();
+        if let Some(libname) = match_path_for_libname(&f) {
+            return Ok(libname);
+        }
+    }
+    anyhow::bail!("no Gurobi library found in {}.", gurobi_libpath.display())
+}
+
+fn get_libname(gurobi_libpath: Option<&Path>) -> String {
+    if let Ok(libname) = env::var("GUROBI_LIBNAME") {
+        return libname;
+    }
+
+    if let Some(path) = gurobi_libpath {
+        match try_guess_libname(path) {
+            Ok(libname) => {
+                println!("inferring libname {libname:?}, set GUROBI_LIBNAME to override.");
+                return libname;
+            }
+            Err(e) => {
+                println!("cargo:warning={e:#}")
+            }
+        }
+    }
+
+    println!(
+        "cargo:warning=Using default libname {DEFAULT_GUROBI_LIBNAME:?}, \
+        set GUROBI_LIBNAME to override.",
+    );
+    String::from(DEFAULT_GUROBI_LIBNAME)
+}
+
+fn try_guess_libpath() -> anyhow::Result<PathBuf> {
     let path = env::var("GUROBI_HOME").context("unable to retrieve value of GUROBI_HOME")?;
 
     // You cannot unset environment variables in the config.toml so this is the next best thing.
-
     if path.is_empty() {
-        anyhow::bail!("GUROBI_HOME is empty")
+        anyhow::bail!("GUROBI_HOME is set to empty string")
     }
 
     let mut path: PathBuf = path.into();
-
-    if cfg!(target_os = "windows") {
-        path.push("bin");
-    } else {
-        path.push("lib");
-    }
-
+    path.push("lib");
     path.canonicalize().with_context(|| {
         format!(
             "GUROBI_HOME points to {} which doesn't exist",
@@ -30,56 +101,61 @@ fn try_gurobi_home() -> anyhow::Result<PathBuf> {
     })
 }
 
-fn try_guess_libname(path: &PathBuf) -> anyhow::Result<String> {
-    for f in std::fs::read_dir(path)? {
-        let raw_f = f?.file_name();
-        let Some(f) = raw_f.to_str() else { continue };
-        let Some(f) = f.strip_prefix("libgurobi") else {
-            continue;
-        };
-        let Some(version) = f.strip_suffix(".so") else {
-            continue;
-        };
-        if version.bytes().all(|b| (b'0'..=b'9').contains(&b)) {
-            return Ok(format!("gurobi{version}"));
-        }
+pub fn main() {
+    if cfg!(feature = "build_script_tests") {
+        return;
     }
-    anyhow::bail!("no libgurobi*.so matches found in {}", path.display())
-}
-
-fn get_lib_name(gurobi_home: Option<&PathBuf>) -> String {
-    if let Ok(value) = env::var("GUROBI_LIBNAME") {
-        return value;
-    }
-    if cfg!(target_os = "linux") {
-        match gurobi_home {
-            Some(p) => match try_guess_libname(p) {
-                Ok(name) => {
-                    println!("inferring libname {name:?}, set GUROBI_LIBNAME to override");
-                    return name;
-                }
-                Err(e) => {
-                    println!("cargo:warning={}", e);
-                }
-            },
-            None => {}
-        }
-    }
-    println!("cargo:warning=using default libname {DEFAULT_GUROBI_LIBNAME:?}, set GUROBI_LIBNAME to override");
-    String::from(DEFAULT_GUROBI_LIBNAME)
-}
-
-fn main() {
     println!("cargo:rerun-if-env-changed=GUROBI_HOME");
     println!("cargo:rerun-if-env-changed=GUROBI_LIBNAME");
 
-    let gurobi_home = try_gurobi_home();
-    match gurobi_home.as_ref() {
+    let libpath = try_guess_libpath();
+    match libpath.as_ref() {
         Ok(path) => {
             println!("cargo:rustc-link-search=native={}", path.display())
         }
-        Err(e) => println!("cargo:warning={:#}", e),
+        Err(e) => println!("cargo:warning={e:#}"),
     }
-    let libname = get_lib_name(gurobi_home.as_ref().ok());
-    println!("cargo:rustc-link-lib=dylib={}", libname);
+    let libname = get_libname(libpath.as_deref().ok());
+    println!("cargo:rustc-link-lib=dylib={libname}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_default_affixes_match_path() {
+        for (name, expected) in [
+            ("libgurobi990.so", Some("gurobi990")),
+            ("libgurobi0.so", Some("gurobi0")),
+            ("libgurobi990_light.so", None),
+            ("libGurobiJni95.so", None),
+        ] {
+            let result = DEFAULT_AFFIXES.match_path_for_libname(name.as_ref());
+            assert_eq!(result.as_deref(), expected);
+        }
+    }
+
+    #[test]
+    fn test_windows_affixes_match_path() {
+        for (name, expected) in [
+            ("gurobi110.lib", Some("gurobi110")),
+            ("gurobi950.lib", Some("gurobi950")),
+            ("gurobi1.lib", Some("gurobi1")),
+            ("gurobi1300655506.lib", Some("gurobi1300655506")),
+            ("gurobi-javadoc.jar", None),
+            ("gurobi.jar", None),
+            ("gurobi.py", None),
+            ("gurobi110.netstandard20.dll", None),
+            ("gurobi110.netstandard20.xml", None),
+            ("gurobi_c++md2017.lib", None),
+            ("gurobi_c++mdd2017.lib", None),
+            ("gurobi_c++mt2017.lib", None),
+            ("gurobi_c++mtd2017.lib", None),
+            ("rootcert.pem", None),
+        ] {
+            let result = WINDOWS_AFFIXES.match_path_for_libname(name.as_ref());
+            assert_eq!(result.as_deref(), expected);
+        }
+    }
 }
